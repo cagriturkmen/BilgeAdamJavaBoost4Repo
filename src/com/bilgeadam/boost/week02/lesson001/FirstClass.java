763085c51e87062bb1efef533024ecf1d8529206
cagriturkmen <c.turkmen2@hotmail.com>
2022-08-15 15:33:27 +0300

    first commit

---
+++ src/com/bilgeadam/boost/week02/lesson001/FirstClass.java
@@ -0,0 +1,11 @@
+package com.bilgeadam.boost.week02.lesson001;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		// this is first java class of boost4 course.
+		
+	}
+
+}
