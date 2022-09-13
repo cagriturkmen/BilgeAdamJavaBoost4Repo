8078ecfa64e13177676b4c63f9ca7c418287f5a1
cagriturkmen <c.turkmen2@hotmail.com>
2022-09-13 13:56:20 +0300

    "sadsadfa

---
+++ src/com/bilgeadam/boost/week04/lesson001/PushOneClassTrial.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.boost.week04.lesson001;
+
+public class PushOneClassTrial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//pushları deniyorum.
+	}
+
+}
---
+++ src/com/bilgeadam/boost/week04/lesson001/SomeClass.java
@@ -0,0 +1,5 @@
+package com.bilgeadam.boost.week04.lesson001;
+
+public class SomeClass {
+
+}
